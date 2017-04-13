9f8467bb1467524bf11be0bdcf6d6ef34e45d34f
ttshimiz <tshimizu@astro.umd.edu>
2017-04-13 19:34:22 +0200

    teams for the 2017 playoffs

--- code/playoff_bet.py
+++ code/playoff_bet.py
@@ -3,62 +3,42 @@ import pandas as pd
 
 # Drafted Players
 taro = ['Draymond Green',
-        'Kevin Love',
-        'Hassan Whiteside',
-        'Russell Westbrook',
-        'Luol Deng']
-        
-vicki = ['Stephen Curry',
-         'Klay Thompson',
-         'Chris Paul',
-         'Kawhi Leonard',
-         'Kevin Durant']
-         
-johnny = ['Lebron James',
-          'Kyrie Irving',
-          'Dwyane Wade',
-          'LaMarcus Aldridge',
-          'Kyle Lowry']
-          
-other = ['DeMar DeRozan',
-         'Damian Lillard',
-         'J.R. Smith',
-         'Goran Dragic',
-         'CJ McCollum',
-         'Serge Ibaka',
-         'Harrison Barnes',
-         'Andre Iguodala',
-         'Shaun Livingston',
-         'Paul George',
-         'Steven Adams',
-         'DeMarre Carroll',
-         'Joe Johnson',
-         'Tristan Thompson',
-         'Bismack Biyombo',
-         'Andrew Bogut',
-         'Mason Plumlee',
-         'Jeff Teague',
-         'Matthew Dellavedova']
-         
-          
+        'Stephen Curry',
+        'Klay Thompson',
+        'Kyrie Irving',
+        'Al Horford']
+
+vicki = ['Lebron James',
+         'Russell Westbrook',
+         'Isaiah Thomas',
+         'LaMarcus Aldridge',
+         'Kyle Lowry']
+
+johnny = ['Kawhi Leonard',
+          'Kevin Durant'
+          'DeMar DeRozan',
+          'Kevin Love',
+          'James Harden']
+
+
 def get_playoff_stats(name):
 
     name_split = name.split()
     first = name_split[0]
     last = name_split[1]
-    
+
     pid = player.get_player(first, last)
     stats = player.PlayerGameLogs(pid, season_type='Playoffs')
-    
+
     return stats.info()
-    
+
 
 def calc_team_totals(team):
-    
+
     totals = pd.DataFrame(columns=['points', 'assists', 'rebounds', 'steals', 'blocks', 'total'],
                           index=team)
     for p in team:
-    
+
         stats = get_playoff_stats(p)
         totals.loc[p, 'points'] = stats['PTS'].sum()
         totals.loc[p, 'assists'] = stats['AST'].sum()
@@ -67,10 +47,10 @@ def calc_team_totals(team):
         totals.loc[p, 'blocks'] = stats['BLK'].sum()
         totals.loc[p, 'total'] = (stats['PTS'].sum() + 1.5*(stats['AST'].sum() + stats['REB'].sum()) +
                                   2.0*(stats['STL'].sum() + stats['BLK'].sum()))
-        
+
     return totals
-    
-    
+
+
 if __name__ == '__main__':
 
     vicki_totals = calc_team_totals(vicki)
@@ -83,7 +63,7 @@ if __name__ == '__main__':
                       2.0*(taro_totals['steals'].sum() + taro_totals['blocks'].sum()))
     johnny_total_pts = (johnny_totals['points'].sum() + 1.5*(johnny_totals['assists'].sum() + johnny_totals['rebounds'].sum()) +
                         2.0*(johnny_totals['steals'].sum() + johnny_totals['blocks'].sum()))
-                              
+
     print 'TEAM\tPTS\tAST\tREB\tSTL\tBLK\tTOTAL'
     print 'VICKI\t{0}\t{1}\t{2}\t{3}\t{4}\t{5}'.format(vicki_totals['points'].sum(),vicki_totals['assists'].sum(),vicki_totals['rebounds'].sum(),
            vicki_totals['steals'].sum(),vicki_totals['blocks'].sum(),
@@ -94,7 +74,6 @@ if __name__ == '__main__':
     print 'JOHNNY\t{0}\t{1}\t{2}\t{3}\t{4}\t{5}'.format(johnny_totals['points'].sum(),johnny_totals['assists'].sum(),johnny_totals['rebounds'].sum(),
            johnny_totals['steals'].sum(),johnny_totals['blocks'].sum(),
            johnny_total_pts)
-    
-    
-    
-        
+
+
+
